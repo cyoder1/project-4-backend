2c9d37256be0547eb6d3d73c1e3a452a43d58473
cyoder1 <yodercamdend@johndeere.com>
2020-11-12 15:13:37 -0600

    updated serveragain

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const constants = require('./constants');
 
 const corsOptions = {
     // origin: ['http://localhost:3000'],
-    origin: ['http://thoughtful-regret.surge.sh/signup'],
+    origin: ['http://thoughtful-regret.surge.sh/'],
     methods: "GET,POST,PUT,DELETE",
     credentials: true, //allows session cookies to be sent back and forth
     optionsSuccessStatus: 200 //legacy browsers
