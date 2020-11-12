fba7a3f401c9737d727a15e7090c01448848f2f8
cyoder1 <yodercamdend@johndeere.com>
2020-11-12 15:07:26 -0600

    updated server

--- migrations/20201111191632-create-project.js
+++ migrations/20201111191632-create-project.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Projects', {
-      id: {
+      project_id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
